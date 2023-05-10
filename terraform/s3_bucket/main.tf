5e4080a7ec8c9c81d590db69e6d0c276db6633a7
lakinduchandula <lakindu.chandula@hotmail.com>
2023-05-10 14:16:48 +0530

    acl ownership police update for s3

--- terraform/s3_bucket/main.tf
+++ terraform/s3_bucket/main.tf
@@ -7,6 +7,7 @@ resource "aws_s3_bucket" "lambda_bucket" {
 resource "aws_s3_bucket_acl" "private_bucket" {
   bucket = aws_s3_bucket.lambda_bucket.id
   acl    = "private"
+  depends_on = [aws_s3_bucket_ownership_controls.s3_bucket_acl_ownership]
 }
 
 resource "aws_s3_object" "lambda_app" {
@@ -16,4 +17,11 @@ resource "aws_s3_object" "lambda_app" {
   source = var.layer_zip_path
 
   etag = filemd5(var.layer_zip_path)
+}
+
+resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership" {
+  bucket = aws_s3_bucket.lambda_bucket.id
+  rule {
+    object_ownership = "ObjectWriter"
+  }
 }
